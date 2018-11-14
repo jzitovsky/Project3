67c0741d3371024c325790fe337fc62d89e6fbde
jzitovsky <joshz@bc02-n09.isis.unc.edu>
2018-11-14 14:58:25 -0500

    commit

--- work/31/0f04cbfdbb9311d4ca07b453f4bc49/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript /home/joshz/Project3/bin/analyzeData.R collaborators.txt words.txt unqWords.txt
--- work/aa/9a091888ff32b372f2c7ece6f3fc4a/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript /home/joshz/Project3/bin/processData.R abs45.txt
