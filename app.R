cdfa502e24cadc0b5330df6eca6f1b77ac4eabd1
jzitovsky <joshz@bc01-n04.isis.unc.edu>
2018-11-19 11:55:36 -0500

    s

--- app.R
+++ app.R
@@ -47,24 +47,26 @@ unimportant = c(stopwords("SMART"), "=", "1", "0", "2", "3", "4", "including", "
 
 
 #building a string that will be the main text (description) of the shiny app
-line1 = "The figures below are based on 441 recent UNC abstracts in cancer-related fields provided by Dr. Biggs. The first figure gives the collaborating institutions that were involved with the greatest number of abstracts, and plots the number of abstracts involving each top collaborator via barplot. By default, the top 10 collaborators are graphed. However, one can adjust the barplot to include anywhere from the top 1 to the top 20 collaborators, by inserting an integer from 1 to 20 in the box below 'Number of collaborators to show'. The second figure is a table giving the 100 words that most frequently appear in abstracts, together with the number of abstract appearences for each word. By default, very generic words such as 'the' and 'have' are hidden. However, filtering is subjective, and what I find to be relevant someone else may find irrelevant, and vice versa. To see all 100 words without filter, uncheck the box at the top corner of the figure titled 'Hide generic words'. One can also see the top words appearing in abstracts associated with a particular collaborator, by selecting a collaborator from the drop-down menu titled 'Collaborators'. Lastly, the filtered word frequencies are plotted in Figure 3 via a word cloud, with words in larger font representing words that appear more frequently in abstracts. Like in Figure 2, one can get plots of word frequencies for each collaborator using a drop-down menu. There is also an option to restrict the maximum words plotted, by moving the slider titled 'Maximum words'."
-line2 = "Note that collaborating instiutions may be treated as separate if they are cited differently and enjoy autonomy from each other, even if they are subsidiaries of the same underlying body. For example, those affiliated with Duke Medical School often say they are affiliated with 'Duke School of Medicine' (and do NOT specifically put 'Duke University' as part of the affiliation) while those affiliated with almost every other department/school of Duke normally say they are affiliated with 'Duke University'. As the school of medicine is cited differently from other departments/schools of Duke, and as the Duke School of Medicine enjoys a parcularly large amount of autonomy compared to other Duke departments/schools regarding its faculty, funding, research goals and administration decisions, we treat 'Duke Medicine' as a separate collaborator from 'Duke University'. Similar logic applies to the medical school of Duke and the hospital of Duke, as well as well as Harvard SPH and Harvard Medical School."
+line1 = "The figures below are based on 441 recent abstracts involving UNC in cancer-related fields provided by Dr. Biggs. The first figure shows the institutions that collaborated with UNC on the greatest number of abstracts, and plots the number of abstracts that each top institution collaborated in via barplot. By default, the top 10 collaborators are graphed. However, one can adjust the barplot to include anywhere from the top 1 to the top 20 collaborators, by inserting an integer from 1 to 20 in the box below 'Number of collaborators to show'. The second figure is a table giving the 100 words that most frequently appear in abstracts, together with the number of abstract appearances for each word. By default, certain words are hidden, such as 'the'. However, filtering is subjective, and what I find to be relevant someone else may find irrelevant, and vice versa. To see all 100 words without filter, uncheck the box at the top corner of the figure titled 'Hide generic words'. One can also see the top words appearing in abstracts associated with a particular collaborator, by selecting a collaborator from the drop-down menu titled 'Collaborators'. Lastly, the filtered word frequencies are plotted in Figure 3 via a word cloud, with words in larger font representing words that appear more frequently in abstracts. Like in Figure 2, one can get plots of word frequencies for each collaborator using a drop-down menu. There is also an option to restrict the maximum words plotted, by moving the slider titled 'Maximum words'."
+
+line2 = "Note that collaborating institutions may be treated as separate if they are cited differently and enjoy autonomy from each other, even if they are subsidiaries of the same underlying body. For example, those affiliated with Duke Medical School often say they are affiliated with 'Duke School of Medicine' (and do NOT specifically put 'Duke University' as part of the affiliation) while those affiliated with almost every other department/school of Duke normally say they are affiliated with 'Duke University'. As the school of medicine is cited differently from other departments/schools of Duke, and as the Duke School of Medicine enjoys a particularly large amount of autonomy compared to other Duke departments/schools regarding its faculty, funding, research goals and administration decisions, we treat 'Duke School of Medicine' as a separate collaborator from 'Duke University'. Similar logic applies to the  hospital of Duke compared to the medical school of Duke,, as well as well as Harvard SPH and Harvard Medical School."
 
 
 #creating shiny app
 shinyApp(
   #creating user interface
   ui = fluidPage(
-    titlePanel("Word Frequencies and Collaborators of Recent UNC Abstracts"),     #title of shiny app
+    titlePanel("Collaborators and Word Frequencies of Recent UNC Abstracts"),     #title of shiny app
     mainPanel(HTML(paste(line1,'<br/>', '<br/>', line2)), width="100px"),         #desciption of shiny app
     br(), br(),                                                                   #adding space between the description and figure
     
     #Controls for first figure: Barplot of top collaborators
     fluidRow(column(12,                                                           
                     div(style = "font-size:25px;",'Figure 1: Top Collaborators')),    #title of figure
+	     br(),
              column(12, 
                     numericInput(inputId = "colNum",                                  #a numeric input that will control the number of collaborators plotted in the barplot
-                                 label = "Number of collaborators to show (give an integer between 1 and 20)",
+                                 label = "Number of collaborators to show (give an integer between 1 and 20):",
                                  value = 10,
                                  min = 1,
                                  max = 20,
@@ -76,9 +78,10 @@ shinyApp(
     #Controls for second figure: Word frequency table
     fluidRow(column(12, 
                     div(style = "font-size:25px;",'Figure 2: Most-Used Words')),     #title of figure
+             br(),
              column(4, 
                     selectInput(inputId = "colInput",                                #a drop-down menu of collaborators - when a collaborator is selected, a word frequency table gets generated based on abstacts involving that collaborator
-                                label = "Collaborator",
+                                label = "Collaborator:",
                                 choices = top10)),
              column(3, offset=5,
                     checkboxInput(inputId = "filter",                                 #a check box that will control whether generic words (e.g. 'the') get filtered out
@@ -90,6 +93,7 @@ shinyApp(
     #Controls for the third figure: Word cloud
     fluidRow(column(12, 
                     div(style = "font-size:25px;",'Figure 3: Word Cloud')),           #title of figure
+	     br(),
              column(4, 
                     sliderInput(inputId = "maxWords",                                 #a slider that will control the maximum words allowed on the plot
                                 label = "Maximum words:",
@@ -98,7 +102,7 @@ shinyApp(
                                 value=100)),
              column(4, offset = 4, 
                     selectInput(inputId = "colInput2",                                #a drop-down menu of collaborators - when a collaborator is selected, a word cloud gets plotted based on abstacts involving that collaborator
-                                label = "Collaborator",
+                                label = "Collaborator:",
                                 choices = top10)),
              column(12, hr(), plotOutput('x3', width='100%', height=500))),           #word cloud output with enlarged height (so words don't get cut off)
     br(), br(), br(), br(), br(), br(), br(), br(), br(),  br(), br(), br(), br(), br(), br(), br(), br(), br(),
@@ -124,7 +128,7 @@ shinyApp(
                  fill=collaborators)) +               #giving each bar a different color
         geom_bar(stat = "identity") +                 #plotting bar geom
         guides(fill=FALSE) +                          #removing legend (we know which bar is for which collaborator via the y-axis)
-        xlab("Collaborator") +                        #improving axes lables
+        xlab("Collaborators") +                        #improving axes lables
         ylab("Number of Abstracts") + 
         coord_flip()                                  #making barplot horizontal
     })
